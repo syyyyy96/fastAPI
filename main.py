77e6ad553fa43ca3292698fc24d76858b5998ea3
syyyyy96 <duddlrk9696@gmail.com>
2026-04-13 23:10:10 +0900

    6일차

--- main.py
+++ main.py
@@ -1,143 +1,36 @@
-#from fastapi import FastAPI
+from llama_cpp import Llama
+from fastapi import FastAPI, Body
 
-# 127.0.0.1:000 서버 실행
-#app = FastAPI()
+llm = Llama(
+    model_path="./models/Llama-3.2-1B-Instruct-Q4_K_M.gguf",
+    n_ctx=4096,
+    n_threads=2,
+    verbose=False,
+    chat_format="llama-3",
+)
 
-# @ ->  python 데코레이터 : 파이썬 함수에 추가적인 기능을 부여하는 문법
-
-# GEt / 요청이 들어오면, root_handler라는 함수를 실행하라 
-#@app.get("/")
-#def root_handler():
-#    return {"ping": "pong"}
-
-
-# Get / hello 요청이 들어오면, hello_handler() 실행 
-#@app.get("/hello")
-#def hello_handler():
-#    return {"message" : "Hello from FastAPI!"}
-
-
-# 전체 사용자 목록 조회 API
-# GET /users
-#@app.get("/users")
-#def get_users_handler():
-#    return {"message" : "Hello from FastAPI!"}
-
-#users = [
-#        {"id": 1, "name": "alex", "job": "student"},
-#        {"id": 2, "name": "bob", "job": "sw engineer"},
-#        {"id": 3, "name": "chris", "job": "barista"},
-#]
-
-# 단일 사용자 데이터 조회 API
-# GET/ users/1 -> 1번 사용자 데이터 조회 
-#@app.get("/users/1")
-#def get_user_one_handler():
-    #return{"id": 1, "name": "alex", "job": "student"}
-
-# GET/ users/2 -> 2번 사용자 데이터 조회 
-#@app.get("/users/2")
-#def get_user_one_handler():
-    #return{"id": 2, "name": "bob", "job": "sw engineer"}
-
-# GET/ users/3 -> 3번 사용자 데이터 조회 
-#@app.get("/users/3")
-#def get_user_one_handler():
-    #return{"id": 3, "name": "chris", "job": "barista"}
-
-# 단일 사용자 데이터 조회 API
-# GET/ users/{user_id} -> {user_id}번 사용자 데이터 조회 
-#@app.get("/users/{user_id")
-#def get_user_handler(user_id: int):
-#    for user in users:
-#        if user["id"]: == user_id:
-#        return user
-    # return None
-
-
-from fastapi import FastAPI, Path, Query
-
-from request import UserCreateRequest
-from response import UserResponse
-
+# 추론을 할 때에는 시스템 프롬프트가 필요
+# 언어 모델에게 규칙을 지정하는 최상위 지시문
+SYSTEM_PROMPT = (
+    "You are a concise assistant. "
+    "Always reply in the same language as the user's input. "
+    "Do not change the language. "
+    "Do not mix languages."
+)
 
 app = FastAPI()
 
-# @ -> Python 데코레이터: 파이썬 함수에 추가적인 기능을 부여하는 문법
-# GET / 요청이 들어오면, root_handler라는 함수를 실행하라
-@app.get("/")
-def root_handler():
-    return {"ping": "pong"}
-
-# GET /hello 요청이 들어오면, hello_handler() 실행
-@app.get("/hello")
-def hello_handler():
-    return {"message": "Hello from FastAPI!"}
-
-# 임시 데이터베이스
-users = [
-    {"id": 1, "name": "alex", "job": "student"},
-    {"id": 2, "name": "bob", "job": "sw engineer"},
-    {"id": 3, "name": "chris", "job": "barista"},
-]
-
-# 전체 사용자 목록 조회 API
-# GET /users
-@app.get("/users")
-def get_users_handler():
-    return users
-
-
-# 사용자 정보 검색 API
-# GET /users/search?name=alex
-# GET /users/search?job=student
-@app.get("/users/search")
-def search_user_handler(
-    name: str | None = Query(None),
-    job: str | None = Query(None),
+@app.post("/chats")
+def generate_chat_handler(
+    # {"user_input": "Python이란 뭐야?"}
+    user_input: str = Body(..., embed=True)
 ):
-    if name is None and job is None:
-        return {"msg": "조회에 사용할 QueryParam이 필요합니다."}
-    return {"name": name, "job": job}
-
-
-# 단일 사용자 데이터 조회 API
-# GET /users/{user_id} -> {user_id}번 사용자 데이터 조회
-@app.get("/users/{user_id}")
-def get_user_handler(
-    user_id: int = Path(..., ge=1),
-):
-    for user in users:
-        if user["id"] == user_id:
-            return user
-
-# 회원 추가 API 
-# POST / users
-@app.post("/users", response_model=UserResponse)
-def create_user_handler(
-    # 1) 사용자 데이터를 넘겨 받는다 + 형식에 맞는지 데이터 유효성 검사 
-    body : UserCreateRequest
-):
-    # 2) 사용자 데이터를 저장한다
-    new_user = {
-        "id" : len(users) + 1,
-        "name" : body.name,
-        "job" : body.job,
-    }
-    users.append(new_user)
-    
-    # 3) 응답을 반환한다. 
-    return new_user
-
-    # return {"name": body.name, "job": body.job}
-    
-
-
-
-
-
-
-# /articles
-# / posts
-# / comments
-# / ??
+    result = llm.create_chat_completion(
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": user_input}
+    ],
+    max_tokens=256,
+    temperature=0.7,
+    )
+    return {"result": result["choices"][0]["message"]["content"].strip()}
